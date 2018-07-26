02e6d4cc7082044eecf6352d55668247244113a7
sanika rake <sanika_rake@persistent.co.in>
2018-07-26 12:37:46 +0530

    method -mymethod is added to display date

--- HelloWorldGit.java
+++ HelloWorldGit.java
@@ -1,4 +1,11 @@
-Class HelloWorld{
+Class HelloWorld
+{
+
+void mymethod()
+{
+sysout("display todays date")
+}
+
 public static void main(String[] args)
 {
 System.out.println("Hello world...welcome to Git");
