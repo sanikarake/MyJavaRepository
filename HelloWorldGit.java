f40f2ae81bd4a4200e13394547fcdb462c1fa03c
sanika rake <sanika_rake@persistent.co.in>
2018-07-26 15:12:07 +0530

    displayDate method called by object

--- HelloWorldGit.java
+++ HelloWorldGit.java
@@ -9,5 +9,7 @@ sysout("display today's date:-")
 public static void main(String[] args)
 {
 System.out.println("Hello world...welcome to Git");
+HelloWorld obj=new HelloWorld();
+wl.displayDate();
 }
 }
